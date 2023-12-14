1a6930dd4cb05f50930bc87268bbec2709a718ed
unknown <sergtsch87@gmail.com>
2023-12-14 23:13:29 +0200

    Added changed to control.py

--- control.py
+++ control.py
@@ -1,1 +1,6 @@
-# some code 
+from dronekit import connect
+
+# Connect to UDP endpoint.
+vehicle = connect('127.0.0.1:14550', wait_ready=True)
+# Use returned Vehicle object to query device state - e.g. to get the mode:
+print(f"Mode: {vehicle.mode.name}")
