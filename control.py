186d0a86783e7bd354320f25476d81ba821ac7e2
unknown <sergtsch87@gmail.com>
2023-12-15 00:30:40 +0200

    Added changes to control.py

--- control.py
+++ control.py
@@ -1,4 +1,6 @@
-from dronekit import connect
+from dronekit import connect, VehicleMode
+from pymavlink import mavutil
+import time
 
 # Connect to UDP endpoint.
 vehicle = connect('127.0.0.1:14550', wait_ready=True)
